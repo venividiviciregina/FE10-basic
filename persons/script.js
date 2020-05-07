872f4c9a64cbba170531f9ed65d4add0320373c3
isalexlog <--global>
2020-05-07 21:39:19 +0200

    Adding persons to the table in index.html

---
+++ persons/script.js
@@ -0,0 +1,33 @@
+let persons = [];
+let averageAge = 0;
+const personsNumber = prompt('Please enter the number of persons:');
+console.log(personsNumber);
+
+
+for (let i = 0; i < personsNumber; i++) {
+    const person = {};
+    person.name = prompt('Please enter name of the ' + (i + 1) + ' person');
+    person.age = prompt('Please enter age of the ' + (i + 1) + 'person');
+    persons[i] = person;
+
+    console.log('Person ' + i + ' Before addition ' + averageAge);
+    averageAge += +person.age; 
+    console.log('Person ' + i + ' After addition ' + averageAge);
+}
+averageAge /= personsNumber;
+
+const sortedPersons = persons.sort(function (a, b) {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+});
+
+let innerHtmlTableBody = '';
+for (let i = 0; i < personsNumber; i++) {
+    innerHtmlTableBody += '<tr><td>' + sortedPersons[i].name + '</td><td>' + sortedPersons[i].age + '</td></tr>';
+    console.log('Person ' + (i + 1) + ': name = ' + sortedPersons[i].name + ' age = ' + sortedPersons[i].age);
+}
+
+document.getElementsByTagName('tbody')[0].innerHTML = innerHtmlTableBody;
+
+console.log('Average age of person:' + averageAge);
