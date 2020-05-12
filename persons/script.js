3de3cc8fc6bd63f101c64fd213913afdb626a231
venividiviciregina <venividiviciregina@gmail.com>
2020-05-12 18:48:45 +0200

    HW 2020-05-07 #34
    3. Add persons to the table (with sorting) right after    adding person via prompt.

--- persons/script.js
+++ persons/script.js
@@ -1,49 +1,52 @@
-let persons = [];
+let personArray = [];
+let age = 0;
 let averageAge = 0;
 const personsNumber = prompt('Please enter the number of persons:');
 console.log(personsNumber);
+let i = 0;
+(function personTable() {
 
 
-for (let i = 0; i < personsNumber; i++) {
     const person = {};
     person.name = prompt('Please enter name of the ' + (i + 1) + ' person');
     person.age = +prompt('Please enter age of the ' + (i + 1) + 'person');
-    persons[i] = person;
+    personArray[i] = person;
+    console.log('Person ' + i + ' Before addition ' + age);
+    age += +person.age;
+    console.log('Person ' + i + ' After addition ' + age);
+    averageAge = age / personArray.length;
+
+    personArray.sort(sortPerson);
+    let html = '';
+    for (let tmp = 0; tmp < personArray.length; tmp++) {
+        html += '<tr><td>' + personArray[tmp].name + '</td><td>' + personArray[tmp].age + '</td></tr>';
+    }
 
 
-    console.log('Person ' + i + ' Before addition ' + averageAge);
-    averageAge += +person.age;
-    console.log('Person ' + i + ' After addition ' + averageAge);
-}
-averageAge /= personsNumber;
+    document.getElementsByTagName('tbody')[0].innerHTML = html;
+    console.log('Average age of person:' + averageAge);
+    document.getElementById('averageAge').innerHTML = 'Average age of ' + personArray.length + ' person ' + averageAge;
+    if (++i < personsNumber) {
+        setTimeout(personTable, 0);
+    }
+
+})();
 
 
-document.getElementById('averageAge').innerHTML = averageAge;
 // const sortedPersons = persons.sort(function (a, b) {
-//         if (a.name < b.name) return -1;
-//         if (a.name > b.name) return 1;
-//         return 0;
-// });
+// // if (a.name < b.name) return -1;
+// // if (a.name > b.name) return 1;
+// // return 0;// });
+//
 
-const sortedPersons = persons.sort(function (a, b) {
-    if (a.age > b.age) return -1;
-    if (a.age < b.age) return 1;
+
+function sortPerson(person1, person2) {
+
+    if (person1.age > person2.age) {
+        return -1;
+    } else if (person1.age < person2.age) {
+        return 1;
+    }
+
     return 0;
-});
-
-
-let innerHtmlTableBody = '';
-for (let i = 0; i < personsNumber; i++) {
-    innerHtmlTableBody += '<tr><td>' + sortedPersons[i].name + '</td><td>' + sortedPersons[i].age + '</td></tr>';
-    console.log('Person ' + (i + 1) + ': name = ' + sortedPersons[i].name + ' age = ' + sortedPersons[i].age);
 }
-
-document.getElementsByTagName('tbody')[0].innerHTML = innerHtmlTableBody;
-
-
-console.log('Average age of person:' + averageAge);
-
-
-
-
-
