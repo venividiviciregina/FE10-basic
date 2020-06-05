ff91025f7f4783c75764be664d8199a3fe440ba1
isalexlog <--global>
2020-06-05 20:15:26 +0200

    feedback project - beginning: html, styles, no scripts

--- feedback/script.js
+++ feedback/script.js
@@ -1,6 +1,1 @@
-let sort = false;
-document.querySelector('button').addEventListener('click', function(e){
-    console.log(e);
-    console.log(numberOfClicks);
-    alert('Button pressed');
-});
+
