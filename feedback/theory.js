bc8aabebc0f2bbfc09da784f3f4ee4d4ac4294ec
isalexlog <--global>
2020-06-05 20:14:44 +0200

    theory - array functions, arrays, objects

--- feedback/theory.js
+++ feedback/theory.js
@@ -29,6 +29,20 @@ function doSomethingSecond(a, b, func1, func2) { // a = 5; b = 10; func1 = funct
 
 console.log(doSomethingSecond(10, 5, sum, minus));
 
+//Arrow function in JavaScript
+
+let a = function(x) {
+    b = x * 2;
+    return b / 2;
+};
+
+let a = (x) => {
+    b = x * 2;
+    return b / 2;
+};
+
+let a = x => x * 2;
+
 let multiply = function(a, b) {
     return a * b;
 }
@@ -50,4 +64,65 @@ function minus(a, b) {
 console.log(
     doSomethingSecond(5, 10, sum, function(a, b) { return a / b; })
 );
+
+// Arrays
+let a = []; // empty array
+
+let b = [1, 2, -5, 0];
+
+console.log(b[0]); 
+
+console.log(b.length);
+
+b.push(10);
+
+console.log(b[b.length - 1]);
+
+b[1] = 3;
+
+console.log(b[1]);
+
+for (let i = 0; i < b.length; i++) {
+    console.log('b[' + i + '] = ' + b[i]);
+};
+
+b.forEach(value => console.log(value));
+
+console.log(typeof b); //object
+
+// Objects
+
+let object = {}; //empty object
+
+console.log(typeof object);
+
+let anotherObject = {
+    name: 'Vasya',
+    age: 25,
+    skills: [
+        'play football', 'eat', 'sleep'
+    ],
+    address: {
+        street: 'Lenina 1',
+        city: 'Leninsk',
+        zipCode: 111000
+    },
+    smoking: false
+};
+
+console.log(anotherObject.name);
+console.log(anotherObject.smoking);
+console.log(anotherObject.skills);
+console.log(anotherObject.address.street);
+
+
+console.log(anotherObject['name']);
+console.log(anotherObject['address']['zipCode']);
+
+let field = 'name';
+console.log(anotherObject[field]);
+
+field = 'address';
+console.log(anotherObject[field]);
+
 
